8cc06719f6a2f868f51870153bc40d737f24fe82
funblaster22 <fogogrl@hotmail.com>
2021-03-07 10:48:05 -0600

    Saving for selected/all window groups

--- popup.js
+++ popup.js
@@ -6,39 +6,62 @@ port.onMessage.addListener(msg => {
   console.log("message received " + msg);
 });
 
-/** Makes a project group out of all tabs open in window */
-function makeProjWin() {
-  chrome.tabs.query({currentWindow: true, groupId: chrome.tabGroups.TAB_GROUP_ID_NONE}, tabs => {
-    const name = prompt('Name of Group');
-    if (name == null) return;
-    chrome.tabs.group({tabIds: tabs.map(tab => tab.id)}, groupId => {
-      // get actual color
-      chrome.tabGroups.get(groupId, group => {
-        chrome.storage.sync.set({[name]: {shortName: "", color: group.color, tabs: tabs.map(tab => tab.url)}});
+function loadProject(name) {
+  // Tab loading logic must occur in background b/c popup gets terminated before adding to group
+  port.postMessage(["load", name]);
+}
+
+async function _addGroup(/** chrome.TabGroup | int */ tabGroup) /** void */ {
+  let shortName = "";
+  if (!Number.isInteger(tabGroup)) {
+    tabGroup = tabGroup.id;
+    shortName = tabGroup.title;
+  } else if (tabGroup !== chrome.tabGroups.TAB_GROUP_ID_NONE) {
+    shortName = (await new Promise(res => chrome.tabGroups.get(tabGroup, group => res(group)))).title;
+  }
+  chrome.storage.sync.get(null, storage => {
+    const allProjects = Object.values(storage).map(val => val.shortName);
+    console.log(shortName, allProjects);
+    if (allProjects.includes(shortName)) {
+      // TODO: don't skip, instead set `name`
+      console.log(`project '${shortName}' already exists, skipping`);
+      return;
+    }
+
+    chrome.tabs.query({currentWindow: true/*, groupId: tabGroup*/}, tabs => {
+      // TODO: for some reason, querying by group ID does not work in chrome 89, try again later
+      tabs = tabs.filter(tab => tab.groupId === tabGroup);
+      const name = prompt('What is the project name of "' + shortName + '"?')
+      if (name == null) return;
+      chrome.tabs.group({tabIds: tabs.map(tab => tab.id), groupId: tabGroup === chrome.tabGroups.TAB_GROUP_ID_NONE ? undefined : tabGroup}, groupId => {
+        // get actual color
+        chrome.tabGroups.get(groupId, group => {
+          chrome.storage.sync.set({[name]: {shortName: shortName, color: group.color, tabs: tabs.map(tab => tab.url)}});
+        });
       });
     });
   });
 }
 
-function loadProject(name) {
-  // Tab loading logic must occur in background b/c popup gets terminated before adding to group
-  port.postMessage(["load", name]);
-
-  /*const tabs = [];
-  chrome.storage.sync.get(name, urls => {
-    for (const url of urls[name]) {
-      chrome.tabs.create({url: url}, tab => {
-        tabs.push(tab.id);
-        console.log(tabs);
-        if (tabs.length === urls[name].length)
-          console.log("FINISHED");
-          chrome.tabs.group({tabIds: tabs});
-        chrome.tabs.group({tabIds: tab.id});
-      });
+function addCurrentGroup(ev) {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    console.assert(tabs.length === 1, "Incorrect number of tabs, expected 1, got " + tabs.length);
+    if (tabs[0].groupId === chrome.tabGroups.TAB_GROUP_ID_NONE) {
+      if (!confirm("No tab group is selected. Add all ungrouped tabs?"))
+        return;
     }
-  });*/
+    _addGroup(tabs[0].groupId);
+  });
 }
 
+function addAllGroups(ev) {
+  chrome.tabGroups.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, groups => {
+    if (groups.length === 0) alert("No groups in active window");
+    for (const grp of groups) {
+      _addGroup(grp.id);
+    }
+  });
+}
 
 /** Show saved project groups in popup gui */
 function renderAllProjects() {
@@ -55,5 +78,7 @@ function renderAllProjects() {
 window.onload = () => {
   console.log("STARTED");
   renderAllProjects();
-  document.getElementById('newProj').onclick = makeProjWin;
+  document.getElementById('newProj').onclick = _addGroup.bind(this, chrome.tabGroups.TAB_GROUP_ID_NONE);
+  document.getElementById('allGrps').onclick = addAllGroups;
+  document.getElementById('curGrp').onclick = addCurrentGroup;
 };
